0d7b9df76733eaa4d6e37fdef132d4c4e579ebae
reham hatem <rha772201@gmail.com>
2025-05-01 20:50:40 +0300

    2nd q

--- lab5/script.js
+++ lab5/script.js
@@ -31,3 +31,110 @@ function stopSlideshow() {
 showSlide();
 //--------------------------------------------------------------------------------------------------------
 
+var tableBody = document.getElementById("studentTable").tBodies[0];
+function addStudent() {
+    let nameInput = document.getElementById("studentName");
+    let gradeInput = document.getElementById("studentGrade");
+    let dept = document.querySelector('input[name="dept"]:checked').value;
+    let nameError = document.getElementById("nameError");
+    let gradeError = document.getElementById("gradeError");
+    nameError.style.display = "none";
+    gradeError.style.display = "none";
+
+    let name = nameInput.value;
+    let grade = parseFloat(gradeInput.value);
+    if (name == "") {
+        nameError.textContent = "Name cannot be empty";
+        nameError.style.display = "block";
+        return;
+    }
+    name = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase();
+
+    let rows = Array.from(tableBody.children);
+    let check= rows.some((elem)=>{
+        return name == elem.cells[0].innerText;
+    });
+    if (check) return;
+    
+    if ( grade < 1 || grade > 100) {
+        gradeError.textContent = "Grade must be between 0 and 100";
+        gradeError.style.display = "block";
+        return;
+    }
+    let row = document.createElement("tr");
+    row.className = grade < 60 ? "failed" : (grade > 60 ? "success" : "");
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${grade}</td>
+        <td>${dept}</td>
+        <td><span class="delete-btn" onclick="deleteRow(this)">  &#128465; </span></td>
+    `;
+    tableBody.appendChild(row);
+    colorTable();
+
+    nameInput.value = "";
+    gradeInput.value = "";
+    
+}
+
+function deleteRow(btn) {
+    btn.parentElement.parentElement.remove();
+}
+
+function sortTable() {
+    let sort = document.getElementById("sortSelect").value;
+    let rows = Array.from(tableBody.children);
+    rows.sort((a, b) => {
+        let aValue, bValue;
+        if (sort == "name") {
+            aValue = a.children[0].textContent;
+            bValue = b.children[0].textContent;
+            return aValue.localeCompare(bValue);
+        } else if (sort == "grade") {
+            aValue = parseFloat(a.children[1].textContent);
+            bValue = parseFloat(b.children[1].textContent);
+            return aValue - bValue;
+        }
+        return 0; 
+    });
+    tableBody.innerHTML = "";
+    rows.forEach(row => tableBody.appendChild(row));
+}
+
+
+let filter = document.getElementById("filterSelect");
+function filterTable() {
+    filter.onchange = ()=>{
+        
+        let rows = document.querySelectorAll("#studentTable tbody tr"); 
+        for(row of rows){
+            let grade = parseFloat(row.children[1].innerText);
+            if (filter.value== "failed"){
+                row.style.display = grade <60 ? "" : "none";
+            }
+            else if (filter.value== "success"){
+                row.style.display = grade >60 ? "" : "none";
+            }
+            else if (filter.value== "all"){
+                row.style.display ="";
+            }
+        }
+    };
+}
+
+function colorTable() {
+        let rows = document.querySelectorAll("#studentTable tbody tr"); 
+        for(row of rows){
+            let grade = parseFloat(row.children[1].innerText);
+            if (grade <60 ){
+                row.style.backgroundColor = "red";
+            }
+            else if (grade >75 ){
+                row.style.backgroundColor = "green";
+            }
+            else if (grade <=75 && grade >60  ){
+                row.style.backgroundColor = "orange";
+            }
+        }
+}
+colorTable();
